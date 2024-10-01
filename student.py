6f30311cf7f312c2afec2fe5b40c142fd849875d
Joe <127773439+joegeorge022@users.noreply.github.com>
2024-10-01 15:37:28 +0530

    Add files via upload

--- student.py
+++ student.py
@@ -1,33 +1,18 @@
-#Python Lab by Sarju S
+#Python Program to get Student Details 
 #Author = Joe George
 #Date = October 1, 2024
 
 print("I love Python")
 
-#The below lines of code shows how to collect input and print it.
-
-name = input("Enter your Student name: ")                       #Here the input is taken as a string.
-roll_no = int(input("Enter your roll number: "))                #Here the input is taken as an integer.
-cgpa = float(input("Enter your cgpa here: "))                   #Here the input is taken as a float.
-cgpa_percentage = cgpa*10                                       #The cgpa is converted to %value. This is possible since the datatype is integer.
+name = input("Enter your Student name: ")
+roll_no = int(input("Enter your roll number: "))
+cgpa = float(input("Enter your cgpa here: "))
+cgpa_percentage = cgpa*10
 
 print("Your name is ",name)
 print("Your roll number is ",roll_no)
 print("Your cgpa is ",cgpa)
 print("Your cgpa percentage is",cgpa_percentage)
-
-
-roll_no = roll_no + 100
-
-print(roll_no)
-
-
-
-
-
-
-
-
 
 
 
