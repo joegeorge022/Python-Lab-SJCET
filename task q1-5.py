a004338bdd9ccfdc8a04d473e0f6d4c39068ef87
Joe <127773439+joegeorge022@users.noreply.github.com>
2024-10-05 15:14:57 +0530

    Add files via upload

---
+++ task q1-5.py
@@ -0,0 +1,86 @@
+"""
+6 October - 12 October
+
+Task:
+
+Write a Python program that asks the user to input their name and age. After receiving the input, display a message that includes the user's name and age.
+Expected Output:
+
+Enter your name: John
+
+Enter your age: 25
+
+Hello, John! You are 25 years old.
+
+--------------------------------------------------------------------------------------------------------------------------
+
+Task:
+
+Write a Python program that stores a string in a variable. Extract a specific part of the string (substring) and then concatenate it with another string. Finally, display the new string.
+Instructions:
+
+    Assign the string "Hello, World!" to a variable.
+    Extract the substring "World" from the string.
+    Concatenate this substring with the string " Everyone!".
+    Print the final concatenated string.
+
+____________________________________________________________________________________________
+Task:
+
+Write a Python program that demonstrates the usage of arithmetic, comparison, and logical operators. Perform a few operations and print the results.
+Instructions:
+
+    Perform addition and division using two integers (a = 10, b = 5).
+    Compare the two integers using the greater-than (>) and equality (==) operators.
+    Use logical operators (and, or) to check conditions.
+    Display the results of all the operations.
+
+Expected Output:
+
+Sum: 15, Division: 2.0
+
+Is a greater than b?: True
+
+Are a and b equal?: False
+
+Logical AND: True
+
+Logical OR: True
+
+__________________________________________________________________________________________
+
+Task:
+
+Write a Python program that uses functions from the math module to perform the following operations on a number provided by the user:
+
+    Find the square root.
+    Calculate the factorial.
+    Raise the number to the power of 2.
+
+Expected Output:
+
+Enter a number: 5
+
+Square root of 5: 2.23606797749979
+
+Factorial of 5: 120
+
+5 raised to power 2: 25.0
+
+--------------------------------------------------------------------------------------------------------------------------
+
+Task:
+
+Write a Python program to print the following string in a specific format (see the output).
+Sample String : "Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are"
+Output :
+
+Twinkle, twinkle, little star,
+	How I wonder what you are!
+		Up above the world so high,
+		Like a diamond in the sky.
+Twinkle, twinkle, little star,
+	How I wonder what you are
+
+--------------------------------------------------------------------------------------------------------------------------
+"""
