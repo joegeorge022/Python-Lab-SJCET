d86e7ebb4b868104684d6a5d6ee8eb5729712d22
Joe <127773439+joegeorge022@users.noreply.github.com>
2024-10-05 15:08:54 +0530

    Add files via upload

---
+++ task5.py
@@ -0,0 +1,24 @@
+'''
+Python Lab Task-5
+Author: Joe George
+Date: October
+'''
+
+# \n creates a new line.
+# \t creates a tab space.
+
+print("Twinkle, twinkle, little star, \n \t How I wonder what you are! \n \t \t Up above the world so high, \n \t \t Like a diamond in the sky. \nTwinkle, twinkle, little star, \n \t How I wonder what you are!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
