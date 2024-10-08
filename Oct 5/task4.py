b960cd03e266bcd2bf56e1534bad06b196149893
Joe <127773439+joegeorge022@users.noreply.github.com>
2024-10-08 14:53:25 +0530

    Add files via upload

---
+++ Oct 5/task4.py
@@ -0,0 +1,33 @@
+'''
+Python Lab Task-4
+Author: Joe George
+Date: October 5
+'''
+
+# math module is imported to perform certain mathematical functions.
+import math
+
+#The below line asks the user for a number input.
+x = int(input("Enter a number: "))
+
+#Below line calculates the square root of the input.
+print("Square root of ",x,":",math.sqrt(x))
+
+#Below line calculates the factorial of the input.
+print("Factorial of",x,":",math.factorial(x))
+
+#Below line calculates the 2nd power of the input.
+print(x,"raised to power 2: ",math.pow(x,2))
+
+
+
+
+
+
+
+
+
+
+
+
+
