ac6795d3c7aef41cf83b439502317b4a3a2d6fe8
Joe <127773439+joegeorge022@users.noreply.github.com>
2024-10-13 08:55:29 +0530

    Update task7.py

--- Oct 8/task7.py
+++ Oct 8/task7.py
@@ -6,17 +6,17 @@ Date: October 8
 Title: Create, concatenate, and print a string and access a sub-string from a given string.
 '''
 
+first_name=input("Enter your first name")       #The first string containing first name.
+last_name=input("Enter your last name")         #The second string containing last name.
 
-str1 = "Joe"                       #The first string containing first name.
-str2 = "George"                    #The second string containing last name.
+full_name=first_name+" "+last_name              #Concatenated the two strings & store result in a new variable.
+print(full_name)                                #Prints the concatenated string.
 
-str3 =str1+" "+str2                #Concatenated the two strings & store result in a new variable.
+length= len(first_name)                         
+print(length)
+extracted_last_name=full_name[:length]          #Uses string slicing to extract the sub-string.
+print(extracted_last_name)
 
-print(str3)                        #Prints the concatenated string.
-
-str4 = str3 [4:]                   #Uses string slicing to extract the sub-string.
-
-print(str4)                        #Printed the sub-string.
 
 
 
