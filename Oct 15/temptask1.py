eba109ce8b3ee00a3d6eec60897dcf9599edf0a4
Joe <127773439+joegeorge022@users.noreply.github.com>
2024-10-15 14:55:17 +0530

    Update temptask1.py

--- Oct 15/temptask1.py
+++ Oct 15/temptask1.py
@@ -9,9 +9,9 @@ Title: Check if a number is greater than 100 or not
 number = int(input("Enter a Number: "))
 
 if number > 100:                                               #Checks if the number is greater than 100 or not.
-    print("The given number:",number,"is greater than 100.")
+    print("The given number",number,"is greater than 100.")
 elif number < 100:
-    print("The given number:",number,"is less than 100.")
+    print("The given number",number,"is less than 100.")
 else:
     print("The given number is 100.")
 
