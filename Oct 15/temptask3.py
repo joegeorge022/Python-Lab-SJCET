b5c64d17bbee69690e6827721e9343812974e895
Joe <127773439+joegeorge022@users.noreply.github.com>
2024-10-15 14:53:25 +0530

    Add files via upload

---
+++ Oct 15/temptask3.py
@@ -0,0 +1,34 @@
+'''
+Python Lab
+Author: Joe George
+Date: October 15
+
+Title: Check if a number is divisible by 5 or not
+'''
+
+number = int(input("Enter a Number: "))
+
+if number%5 == 0:
+    print("The given number",number,"is divisible by 5.")
+else:
+    print("The given number",number,"is not divisible by 5.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
