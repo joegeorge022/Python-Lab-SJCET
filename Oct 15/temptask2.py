8e06dc59c5cd4c4f7ee845b48253c51ae38fa0e3
Joe <127773439+joegeorge022@users.noreply.github.com>
2024-10-15 14:52:45 +0530

    Add files via upload

---
+++ Oct 15/temptask2.py
@@ -0,0 +1,39 @@
+'''
+Python Lab
+Author: Joe George
+Date: October 15
+
+Title: Check if a person is eligible for driving license
+'''
+
+age = int(input("Enter your age: "))
+
+if age >= 16:
+    print("The person is eligible for a driving license.")
+else:
+    print("The person is not eligible for a driving license.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
