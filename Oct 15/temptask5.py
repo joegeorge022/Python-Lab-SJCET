1c1a2899b0bacd15a23a9e3fc6d746517592b651
Joe <127773439+joegeorge022@users.noreply.github.com>
2024-10-15 14:54:00 +0530

    Add files via upload

---
+++ Oct 15/temptask5.py
@@ -0,0 +1,31 @@
+'''
+Python Lab
+Author: Joe George
+Date: October 15
+
+Title: Traffic instructions based on traffic light.
+'''
+
+color = (input("Enter the TRAFFIC LIGHT COLOR: "))
+
+if color == "red":
+    print("STOP")
+elif color == "green":
+    print("GO")
+else:
+    print("WAIT")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
