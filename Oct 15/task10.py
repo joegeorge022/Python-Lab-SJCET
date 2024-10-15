8a13abe9bf8f1b0209d7e3583a3b0bed9ac69fa3
Joe <127773439+joegeorge022@users.noreply.github.com>
2024-10-15 16:17:53 +0530

    Add files via upload

---
+++ Oct 15/task10.py
@@ -0,0 +1,36 @@
+'''
+Python Lab
+Author: Joe George
+Date: October 15
+
+Title: Program to generate n odd numbers.
+'''
+
+
+limit = int(input("Enter the limit: "))
+odd_number = 1
+count=0
+while count<limit:
+    print(odd_number,"\t",end="")
+    count+=1
+    odd_number+=2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Oct 15/task11.py
@@ -0,0 +1,27 @@
+'''
+Python Lab
+Author: Joe George
+Date: October 15
+
+Title: Program to generate n odd numbers upto the number n.
+'''
+
+
+limit = int(input("Enter the limit: "))
+odd_number = 1
+while odd_number<=limit:
+    print(odd_number,"\t",end="")
+    odd_number+=2
+
+
+
+
+
+
+
+
+
+
+
+
+
