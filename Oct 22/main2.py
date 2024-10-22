b468d11734808b2e335f1523d18d97e352c4885e
Joe <127773439+joegeorge022@users.noreply.github.com>
2024-10-22 14:55:40 +0530

    Update main2.py

--- Oct 22/main2.py
+++ Oct 22/main2.py
@@ -10,12 +10,11 @@ temp = int(input("Enter temperature: "))
 Confirm = input("Is this in (C)elsius or (F)ahrenheit: ")
 
 if Confirm == "C":
-    print(temp,"Celsius is",temp*(9/5)+32,"Fahrenheit.",)
+    print(temp,"Celsius is",temp*(9/5)+32,"Fahrenheit.")
+elif Confirm == "F":
+    print(temp, "Fahrenheit is ", 5 / 9 * (temp - 32), "Celsius.")
 else:
-    print(temp,"Fahrenheit is ",0.0," Celsius.")
-
-
-
+    print("Unit is not valid")
 
 
 
