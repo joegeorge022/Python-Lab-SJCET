74a1da34987add591b7fef5e7b3f2da42f2075af
Joe <127773439+joegeorge022@users.noreply.github.com>
2024-10-22 15:09:49 +0530

    Update main2.py

--- Oct 22/main2.py
+++ Oct 22/main2.py
@@ -6,21 +6,15 @@ Date: October 22
 Title: Convert temperature values back and forth between Celsius and Fahrenheit.
 '''
 
-temp = int(input("Enter temperature: "))
-Confirm = input("Is this in (C)elsius or (F)ahrenheit: ")
+temp = int(input("Enter temperature: "))                                          #Asks the user to enter input
+Confirm = input("Is this in (C)elsius or (F)ahrenheit: ")                         #Checks if temperature is in Celsius or Fahrenheit.
 
-if Confirm == "C":
-    print(temp,"Celsius is",temp*(9/5)+32,"Fahrenheit.")
-elif Confirm == "F":
-    print(temp, "Fahrenheit is ", 5 / 9 * (temp - 32), "Celsius.")
+if Confirm == "C"or Confirm == "c":
+    print(temp,"Celsius is",temp*(9/5)+32," Fahrenheit.")                         #Converts Celsius to Fahrenheit.
+elif Confirm == "F"or Confirm == "f":
+    print(temp, "Fahrenheit is ", 5 / 9 * (temp - 32), "Celsius.")                #Converts Fahrenheit to Celsius.
 else:
-    print("Unit is not valid")
-
-
-
-
-
-
+    print("Unit is not valid")                                                    #You entered the wrong unit mate. How dumb are you?? I even made it case insensitive🥹.
 
 
 
