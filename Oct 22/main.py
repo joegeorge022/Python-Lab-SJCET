51b791af57d76d805a383cbdd06aaae991c2b963
Joe <127773439+joegeorge022@users.noreply.github.com>
2024-10-28 18:08:00 +0530

    Update main.py

--- Oct 22/main.py
+++ Oct 22/main.py
@@ -11,11 +11,11 @@ num2 = int(input("Enter Number 2: "))
 num3 = int(input("Enter Number 3: "))
 
 
-if num1>=num2 and num1>=num3:
+if num1>=num2 and num1>=num3:                                    #Checks if 1 is the largest number
     print("The given number",num1," is largest.")
-elif num2>=num3 and num2>=num1:
+elif num2>=num3 and num2>=num1:                                  #Checks if 2 is the largest number
     print("The given number",num2,"is largest.")
-else:
+else:                                                            #If neither 1 nor 2 is the largest number then 3 is the largest number
     print("The given number",num3,"is largest.")
 
 
