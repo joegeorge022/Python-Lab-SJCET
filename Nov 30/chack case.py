ab4bc682157a9734ad9f0485a1f8822d0518d5c6
Joe <127773439+joegeorge022@users.noreply.github.com>
2024-11-30 10:47:17 +0530

    Update chack case.py

--- Nov 30/chack case.py
+++ Nov 30/chack case.py
@@ -10,13 +10,16 @@ string_input = input("Enter your string: ")
 # Sets the value of number of Uppercase & Lowercase characters = 0 at beginning
 upper_count = 0
 lower_count = 0
+number_count = 0
 
-#
 for i in range(len(string_input)):
     if  string_input[i].isupper() is True:    # This conditional checks for uppercase letters
-        upper_count +=1     #Adds +1 to number of Uppercase characters
-    else:
-        lower_count +=1     #Adds +1 to number of lowercase characters
+        upper_count +=1     #Adds +1 to number of Uppercase characters.
+    elif string_input[i].islower() is True:
+        lower_count +=1     #Adds +1 to number of lowercase characters.
+    elif string_input[i].isnumeric() is True:
+        number_count +=1    #Adds +1 to number of numeric characters.
 
 print(f"The number of Uppercase characters in the string is: {upper_count}")
 print(f"The number of lowercase characters in the string is: {lower_count}")
+print(f"The number of Numeric characters in the string is: {number_count}")
